e494d35e62216998e04c4c14c15558023c627d25
Marcus Davis <TheWebGuru2@MacBook-Pro.local>
2018-04-16 18:45:47 -0400

    removal of sudo

--- installos.sh
+++ installos.sh
@@ -3,33 +3,33 @@ echo "Installing Variety Apt Repo"
 echo "******************************************"
 echo ""
 
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:peterlevi/ppa
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:peterlevi/ppa
 
 echo "******************************************"
 echo "Installing Sublime Text"
 echo "******************************************"
 echo ""
 
-sudo apt-get install -y apt-transport-https
+apt-get install -y apt-transport-https
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt-get install -y sublime-text
+apt update
+apt-get install -y sublime-text
 
 echo "******************************************"
 echo "Installing Lightdm, Openbox, and Xserver etc"
 echo "******************************************"
 echo ""
 
-sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nemo sakura lxappearance lxappearance-obconf feh tint2 variety variety-slideshow nitrogen plank chromium-browser firefox filezilla wget git
+apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nemo sakura lxappearance lxappearance-obconf feh tint2 variety variety-slideshow nitrogen plank chromium-browser firefox filezilla wget git
 
 echo "******************************************"
 echo "Installing Intel Microcode"
 echo "******************************************"
 echo ""
 
-sudo apt install -y intel-microcode
+apt install -y intel-microcode
 
 echo "******************************************"
 echo "Creating Autostart"
@@ -56,20 +56,20 @@ echo "******************************************"
 echo ""
 
 wget -o ~/obmenu-generator_0.66-1_all.deb https://download.opensuse.org/repositories/home:/Head_on_a_Stick:/obmenu-generator/Debian_8.0/all/obmenu-generator_0.66-1_all.deb
-sudo apt install -y ./obmenu-generator_0.66-1_all.deb
+apt install -y ./obmenu-generator_0.66-1_all.deb
 
 echo "******************************************"
 echo "Setting up Right Click Menu"
 echo "******************************************"
 echo ""
 
-sudo obmenu-generator -s -c
-sudo sed -i 's/geany/subl/g' ~/.config/obmenu-generator/schema.pl
-sudo sed -i 's/geany/subl/g' ~/.config/obmenu-generator/config.pl
-sudo sed -i 's/xterm/sukara/g' ~/.config/obmenu-generator/schema.pl
-sudo sed -i 's/xterm/sukara/g' ~/.config/obmenu-generator/config.pl
-sudo sed -i 's|xdg-open http://|chromium-browser|g' ~/.config/obmenu-generator/schema.pl
-sudo obmenu-generator -s -c
+obmenu-generator -s -c
+sed -i 's/geany/subl/g' ~/.config/obmenu-generator/schema.pl
+sed -i 's/geany/subl/g' ~/.config/obmenu-generator/config.pl
+sed -i 's/xterm/sakura/g' ~/.config/obmenu-generator/schema.pl
+sed -i 's/xterm/sakura/g' ~/.config/obmenu-generator/config.pl
+sed -i 's|xdg-open http://|chromium-browser|g' ~/.config/obmenu-generator/schema.pl
+obmenu-generator -s -c
 
 echo "******************************************"
 echo "Installing Tint2 Themes"
@@ -78,7 +78,7 @@ echo ""
 
 cd ~
 git clone https://github.com/arcolinux/arcolinux-tint2-themes.git
-sudo cp -a ~/arcolinux-tint2-themes/*rc ~/.config/tint2/
+cp -a ~/arcolinux-tint2-themes/*rc ~/.config/tint2/
 
 echo "******************************************"
 echo "Installing Plank Themes"
@@ -96,7 +96,7 @@ echo ""
 
 cd ~
 git clone https://github.com/horst3180/arc-icon-theme.git
-sudo mv ./arc-icon-theme/Arc/ /usr/share/icons/
+mv ./arc-icon-theme/Arc/ /usr/share/icons/
 
 echo "******************************************"
 echo "Cleaning up"
