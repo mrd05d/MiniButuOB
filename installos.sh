9646af531d81d2228716d1e1bd82d29b7165ba3e
Marcus Davis <TheWebGuru2@MacBook-Pro.local>
2018-04-16 17:29:38 -0400

    Added Silent switch on apt-get installs

--- installos.sh
+++ installos.sh
@@ -1,4 +1,8 @@
-sudo apt install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nemo sakura lxappearance lxappearance-obconf feh tint2 variety nitrogen plank chromium-browser firefox filezilla git
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:peterlevi/ppa
+
+
+sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nemo sakura lxappearance lxappearance-obconf feh tint2 variety nitrogen plank chromium-browser firefox filezilla git
 
 
 mkdir ~/.config/openbox
@@ -9,14 +13,14 @@ echo "plank &" >> autostart
 
 sed -i 's/bottom/top/g' ~/.config/tint2/tint2rc
 
-sudo apt-get install apt-transport-https
+sudo apt-get install -y apt-transport-https
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt-get install sublime-text
+sudo apt-get install -y sublime-text
 
 wget https://download.opensuse.org/repositories/home:/Head_on_a_Stick:/obmenu-generator/Debian_8.0/all/obmenu-generator_0.66-1_all.deb
-sudo apt install ./obmenu-generator_0.66-1_all.deb
+sudo apt install -y ./obmenu-generator_0.66-1_all.deb
 
 sudo obmenu-generator -s -c
 sudo sed -i 's/geany/subl/g' schema.pl
@@ -35,10 +39,6 @@ sudo cp -a ~/Plank-Themes/Plank\ Themes/* /usr/share/plank/themes/
 
 git clone https://github.com/horst3180/arc-icon-theme.git
 sudo mv ./arc-icon-theme/Arc/ /usr/share/icons/
-
-
-
-
 
 sudo rm -r ~/arcolinux-tint2-themes
 sudo rm -r ~/obmenu-generator_0.66-1_all.deb
